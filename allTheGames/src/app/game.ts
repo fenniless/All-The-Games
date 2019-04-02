a4091998190060c82fb71c94a2197f94322f6870
Fenniless <fenniless@gmail.com>
2019-04-02 00:08:10 -0400

    doing angular Hero tutorial but replacing heros with games

---
+++ allTheGames/src/app/game.ts
@@ -0,0 +1,9 @@
+export class Game {
+    id: number;
+    name: string;
+    overview: string;
+    releaseDate: number;
+    players: number;
+    platform: string;
+    rating: string;
+}
---
+++ allTheGames/src/app/game/game.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../game';
+import { GAMES} from '../mock_games';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+  game = GAMES;
+  selectedGame: Game;
+  // @ts-ignore
+  // game: Game = {
+  //   id: 1,
+  //   name: 'Super Mario Bros',
+  //   overview: ''
+  // };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
